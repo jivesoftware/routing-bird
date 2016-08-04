7849b39f4092c6acf7c6fa83186330964847cdc7
Kevin Karpenske <kevin.karpenske@jivesoftware.com>
2016-08-04 11:39:08 -0500

    mlogger 0.18; require at least 1 sample in timer health

--- routing-bird-health/src/main/java/com/jivesoftware/os/routing/bird/health/checkers/TimerHealthChecker.java
+++ routing-bird-health/src/main/java/com/jivesoftware/os/routing/bird/health/checkers/TimerHealthChecker.java
@@ -56,13 +56,15 @@ public class TimerHealthChecker implements HealthChecker<Timer>, ResettableHealt
     private String healthString(Timer timer) {
         StringBuilder sb = new StringBuilder();
         sb.append(" samples:").append(timer.getSampleCount());
-        sb.append(" mean:").append(timer.getMean());
-        sb.append(" 50th:").append(timer.get50ThPercentile());
-        sb.append(" 75th:").append(timer.get75ThPercentile());
-        sb.append(" 90th:").append(timer.get90ThPercentile());
-        sb.append(" 95th:").append(timer.get95ThPercentile());
-        sb.append(" 99th:").append(timer.get99ThPercentile());
-        sb.append(" max:").append(timer.getMax());
+        if (timer.getSampleCount() > 0) {
+            sb.append(" mean:").append(timer.getMean());
+            sb.append(" 50th:").append(timer.get50ThPercentile());
+            sb.append(" 75th:").append(timer.get75ThPercentile());
+            sb.append(" 90th:").append(timer.get90ThPercentile());
+            sb.append(" 95th:").append(timer.get95ThPercentile());
+            sb.append(" 99th:").append(timer.get99ThPercentile());
+            sb.append(" max:").append(timer.getMax());
+        }
         return sb.toString();
     }
 
@@ -83,13 +85,15 @@ public class TimerHealthChecker implements HealthChecker<Timer>, ResettableHealt
         @Override
         public HealthCheckResponse call() throws Exception {
             double health = 1.0f;
-            health = Math.min(health, HealthCheckUtil.zeroToOne(config.getMeanMax(), 0, timer.getMean()));
-            health = Math.min(health, HealthCheckUtil.zeroToOne(config.getVarianceMax(), 0, Math.abs(timer.getVariance())));
-            health = Math.min(health, HealthCheckUtil.zeroToOne(config.get50ThPecentileMax(), 0, timer.get50ThPercentile()));
-            health = Math.min(health, HealthCheckUtil.zeroToOne(config.get75ThPecentileMax(), 0, timer.get75ThPercentile()));
-            health = Math.min(health, HealthCheckUtil.zeroToOne(config.get90ThPecentileMax(), 0, timer.get90ThPercentile()));
-            health = Math.min(health, HealthCheckUtil.zeroToOne(config.get95ThPecentileMax(), 0, timer.get95ThPercentile()));
-            health = Math.min(health, HealthCheckUtil.zeroToOne(config.get99ThPecentileMax(), 0, timer.get99ThPercentile()));
+            if (timer.getSampleCount() > 0) {
+                health = Math.min(health, HealthCheckUtil.zeroToOne(config.getMeanMax(), 0, timer.getMean()));
+                health = Math.min(health, HealthCheckUtil.zeroToOne(config.getVarianceMax(), 0, Math.abs(timer.getVariance())));
+                health = Math.min(health, HealthCheckUtil.zeroToOne(config.get50ThPecentileMax(), 0, timer.get50ThPercentile()));
+                health = Math.min(health, HealthCheckUtil.zeroToOne(config.get75ThPecentileMax(), 0, timer.get75ThPercentile()));
+                health = Math.min(health, HealthCheckUtil.zeroToOne(config.get90ThPecentileMax(), 0, timer.get90ThPercentile()));
+                health = Math.min(health, HealthCheckUtil.zeroToOne(config.get95ThPecentileMax(), 0, timer.get95ThPercentile()));
+                health = Math.min(health, HealthCheckUtil.zeroToOne(config.get99ThPecentileMax(), 0, timer.get99ThPercentile()));
+            }
             return new HealthCheckResponseImpl(config.getName(), Math.max(health, 0f), healthString(timer), description, resolution, time);
         }
     }
