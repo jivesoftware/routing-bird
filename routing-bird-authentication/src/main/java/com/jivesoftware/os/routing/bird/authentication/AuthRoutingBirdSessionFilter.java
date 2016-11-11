c7c2f498ac4e154602be1ed37bcf8086dc68fdee
Kevin Karpenske <kevin.karpenske@jivesoftware.com>
2016-11-11 11:56:37 -0600

    use root path in session cookies

--- routing-bird-authentication/src/main/java/com/jivesoftware/os/routing/bird/authentication/AuthRoutingBirdSessionFilter.java
+++ routing-bird-authentication/src/main/java/com/jivesoftware/os/routing/bird/authentication/AuthRoutingBirdSessionFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.NewCookie;
 
@@ -19,7 +20,17 @@ public class AuthRoutingBirdSessionFilter implements ContainerResponseFilter {
         for (String name : requestContext.getPropertyNames()) {
             if (name.startsWith("rb_session_token")) {
                 Object rbSessionToken = requestContext.getProperty(name);
-                headers.add("Set-Cookie", new NewCookie(name, rbSessionToken.toString()));
+                headers.add("Set-Cookie",
+                    new NewCookie(name,
+                        rbSessionToken.toString(),
+                        "/",
+                        null,
+                        Cookie.DEFAULT_VERSION,
+                        null,
+                        NewCookie.DEFAULT_MAX_AGE,
+                        null,
+                        false,
+                        false));
             }
         }
     }
